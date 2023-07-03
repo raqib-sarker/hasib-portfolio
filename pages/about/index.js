2df63fa097e89835429e698ff8569654469f9fec
coderhasib23 <coderhasib23@gmail.com>
2023-07-03 20:51:29 +0600

    service area added

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+//icons
 import {
   FaHtml5,
   FaCss3,
@@ -11,92 +14,64 @@ import {
   SiAdobephotoshop,
 } from "react-icons/hi2";
 
-//  data
-const aboutData = [
-  {
-    title: "skills",
-    info: [
-      {
-        title: "Web Development",
-        icons: [
-          <FaHtml5 key="html5" />,
-          <FaCss3 key="css3" />,
-          <FaJs key="js" />,
-          <FaReact key="react1" />,
-          <FaReact key="react2" />,
-          <SiFramer key="framer" />,
-          <FaWordpress key="wordpress" />,
-        ],
-      },
-      {
-        title: "UI/UX Design",
-        icons: [
-          <FaFigma key="figma" />,
-          <SiAdobexd key="adobexd" />,
-          <SiAdobephotoshop key="photoshop" />,
-        ],
-      },
-    ],
-  },
-  {
-    title: "awards",
-    info: [
-      {
-        title: "Webby Awards - Honoree",
-        stage: "2011 - 2012",
-        key: "webby",
-      },
-      {
-        title: "Adobe Design Achievement Awards - Finalist",
-        stage: "2009 - 2010",
-        key: "adobe",
-      },
-    ],
-  },
-  {
-    title: "experience",
-    info: [
-      {
-        title: "UX/UI Designer - XYZ Company",
-        stage: "2012 - 2023",
-        key: "uxui",
-      },
-      {
-        title: "Web Developer - ABC Agency",
-        stage: "2010 - 2012",
-        key: "webdev",
-      },
-      {
-        title: "Intern - DEF Corporation",
-        stage: "2008 - 2010",
-        key: "intern",
-      },
-    ],
-  },
-  {
-    title: "credentials",
-    info: [
-      {
-        title: "Web Development - ABC University, LA, CA",
-        stage: "2011",
-        key: "webdev-education",
-      },
-      {
-        title: "Computer Science Diploma - AV Technical Institute",
-        stage: "2009",
-        key: "cs-diploma",
-      },
-      {
-        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
-        stage: "2006",
-        key: "graphic-designer",
-      },
-    ],
-  },
-];
+//components
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      {/* about me section */}
+      <section className="flex items-center xl:h-screen font-poppins dark:bg-gray-800">
+        <div className="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div className="flex flex-wrap">
+            <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 hidden xl:flex justify-end xl:max-w-none">
+              <div className="relative lg:max-w-md">
+                <Image
+                  src={"/avatar6.png"}
+                  width={537}
+                  height={678}
+                  alt=""
+                  className="translate-z-0  w-full h-full"
+                />
+              </div>
+            </div>
+            <div className="w-full px-6 mb-10 lg:w-1/2 lg:mb-0 ">
+              <div className="pl-4 mb-6 border-l-4 border-blue-500 mt-36 lg:mt-0">
+                <span className="text-sm text-gray-600 uppercase dark:text-gray-400">
+                  Who am I?
+                </span>
+                <h1 className="mt-2 md:text-5xl dark:text-gray-300 text-white font-bold text-4xl xl:text-6xl">
+                  About Me
+                </h1>
+              </div>
+              <p className="mb-6 text-base leading-7 text-white text-justify">
+                Hello, my name is Hasib Sarker. I am a junior front-end
+                developer with a strong focus on working with front-end
+                technologies, particularly React. <br />I have a solid
+                understanding of the latest front-end technologies and
+                frameworks, with a particular expertise in React. I believe in
+                continuously learning and staying up-to-date with emerging
+                trends to deliver cutting-edge solutions. <br />
+                My biggest strength as a front-end developer lies in my ability
+                to tackle complex problems and find innovative solutions. I have
+                a strong analytical mindset, which allows me to break down
+                challenges into smaller, manageable tasks. This enables me to
+                deliver high-quality code and meet project deadlines
+                effectively.
+              </p>
+              <Link
+                className="px-4 py-2 text-gray-100 bg-[#F13024] rounded dark:bg-red-400 dark:hover:bg-red-500 hover:bg-red-600"
+                href="/services"
+              >
+                See Services
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default About;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,99 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="relative pt-16 bg-blueGray-50 mt-6">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap items-center">
+          <div className="hidden xl:flex justify-end xl:max-w-none w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4  mr-auto ml-auto -mt-78">
+            <div className="">
+              <Image
+                src={"/bulb.png"}
+                width={100}
+                height={100}
+                alt=""
+                className=" align-middle rounded-t-lg"
+              />
+              <blockquote className="relative p-8 mb-4">
+                <svg
+                  preserveAspectRatio="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 583 95"
+                  className="absolute left-0 w-full block h-95-px -top-94-px"
+                >
+                  {/*  */}
+                </svg>
+                <h4 className="text-xl font-bold text-white">
+                  Great for your awesome project
+                </h4>
+                <p className="text-md font-light mt-2 text-white text-justify">
+                  Building a web app has never been easier than assembling
+                  pre-made components. From landing pages to interactive
+                  dashboards, you can effortlessly customize and develop your
+                  web applications.
+                </p>
+              </blockquote>
+            </div>
+          </div>
+
+          <div className="w-full md:w-6/12 px-4 mt-6 xl:mt-0">
+            <div className="flex flex-wrap">
+              <div className="w-full md:w-6/12 px-4">
+                <div className="relative flex flex-col mt-4">
+                  <div className="px-4 py-5 flex-auto">
+                    <h6 className="text-xl mb-1 font-semibold">
+                      Custom Web App
+                    </h6>
+                    <p className="mb-4 text-blueGray-500">
+                      I am specialize in developing tailor-made web applications
+                      that meet your specific business requirements.
+                    </p>
+                  </div>
+                </div>
+                <div className="relative flex flex-col min-w-0">
+                  <div className="px-4 py-5 flex-auto">
+                    <h6 className="text-xl mb-1 font-semibold">
+                      Responsive Design
+                    </h6>
+                    <p className="mb-4 text-blueGray-500">
+                      I create visually appealing and mobile-friendly web
+                      designs that adapt seamlessly to different screen sizes
+                      and devices.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full md:w-6/12 px-4">
+                <div className="relative flex flex-col min-w-0 mt-4">
+                  <div className="px-4 py-5 flex-auto">
+                    <h6 className="text-xl mb-1 font-semibold">E-commerce</h6>
+                    <p className="mb-4 text-blueGray-500">
+                      Boost your online business with my robust e-commerce
+                      solutions. From secure payment gateways to intuitive
+                      product.
+                    </p>
+                  </div>
+                </div>
+                <div className="relative flex flex-col min-w-0">
+                  <div className="px-4 py-5 flex-auto">
+                    <h6 className="text-xl mb-1 font-semibold">
+                      Maintenance and Support
+                    </h6>
+                    <p className="mb-4 text-blueGray-500">
+                      To ensure your application remains up-to-date, secure, and
+                      performs optimally.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
