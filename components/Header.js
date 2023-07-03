c2b917159aa554bca2d75e150b9ba576348791f6
coderhasib23 <coderhasib23@gmail.com>
2023-07-03 19:00:09 +0600

    header added

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,42 @@
+// next/image
+import Image from "next/image";
+
+// next/router
+import { useRouter } from "next/router";
+
+// components
+import Socials from "../components/Socials";
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.push("/");
+  };
+
+  return (
+    <header className="absolute z-30 w-full flex items-center">
+      <div className="container mx-auto">
+        <div
+          className="flex flex-col lg:flex-row justify-between items-center gap-y-6
+py-8"
+        >
+          <div>
+            <a onClick={handleLogoClick} style={{ cursor: "pointer" }}>
+              <Image
+                src="/logo-up.png"
+                alt=""
+                width={220}
+                height={48}
+                priority={true}
+              />
+            </a>
+          </div>
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,32 @@
+import {
+  RiYoutubeLine,
+  RiLinkedinLine,
+  RiFacebookLine,
+  RiGithubLine,
+  RiWhatsappLine,
+} from "react-icons/ri";
+import Link from "next/link";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-xl lg:mr-40">
+      <Link href="https://linkedin.com/in/techhasib" passHref>
+        <RiLinkedinLine className="hover:text-accent transition-all duration-300" />
+      </Link>
+      <Link href="https://github.com/techhasib" passHref>
+        <RiGithubLine className="hover:text-accent transition-all duration-300" />
+      </Link>
+      <Link href="https://www.facebook.com/techhasib" passHref>
+        <RiFacebookLine className="hover:text-accent transition-all duration-300" />
+      </Link>
+      <Link href="https://www.youtube.com/techhasib" passHref>
+        <RiYoutubeLine className="hover:text-accent transition-all duration-300" />
+      </Link>
+      <Link href="http://wa.me/+8801688494480" passHref>
+        <RiWhatsappLine className="hover:text-accent transition-all duration-300" />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
